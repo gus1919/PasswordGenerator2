b385c5ceb2b6ca11aad91612c37ddb000bd78fdd
Justin Oeters <justin.oeters@gmail.com>
2021-08-07 15:52:51 -0400

    Password Variables

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//  1. Upon clicking Generate Password button we are prompted to 
+//       a. password length 8-128 characters
+//       b numbers
+//       c uppercase
+//       d lowercase
+//  2. create the random password
+//  3. write password to password input
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// possible arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  passwordText.value = password;
+// Variable Declarations 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-}
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // password between 8 and 128 characters 
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-128 characters.  Please enter a proper length");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+   
+
+
